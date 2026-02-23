4ea2f1d6c086fa7d1ac76aa449184f384b8d774f
Amrik Malhans <60037787+amrikmalhans@users.noreply.github.com>
2026-02-22 22:49:20 -0500

    feat: update HomePage to display destinations and simplify layout

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,31 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
-  const headers = await getHeaders()
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+  const { docs: destinations } = await payload.find({
+    collection: 'destinations',
+  })
 
   return (
-    <div className="home">
+    <section className="home">
       <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
+        <h1>Home</h1>
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
-    </div>
+      <section className="destinations">
+        <ul>
+          {destinations.map((destination) => (
+            <li key={destination.id}>
+              <h2>{destination.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </section>
   )
 }
