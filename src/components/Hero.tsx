1613f179fa61fb6021cad241bfcf274d4a95ae1b
Amrik Malhans <60037787+amrikmalhans@users.noreply.github.com>
2026-02-23 20:43:39 -0500

    feat(frontend): add styled hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero__title">Northern lights forecast</h1>
+      <p className="hero__description">
+        Discover when and where to see the northern lights with a 3-day forecast for your location in
+        Norway, or be inspired by top aurora destinations.
+      </p>
+    </section>
+  )
+}
+
+export default Hero
